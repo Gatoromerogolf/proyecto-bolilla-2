c4468217c20a16dd40bb0efb9d021663ebf8fd09
Gatoromerogolf <ruben.e.garcia@gmail.com>
2023-12-22 18:46:51 -0300

    ventana

--- js/saludo.js
+++ js/saludo.js
@@ -1,4 +1,1 @@
-var show
-if (show !== 1)
-{alert ("Bienvenido a la web de bolilla.   Tenés la fortuna de pertenecer a este grupo exclusivo.   En esta web tendrás toda la información de la actividad del grupo: histórica, presente y futura.   Que Dios te ayude " + show);
-show = 1;}
+alert ("Bienvenido a la web de bolilla.   Tenés la fortuna de pertenecer a este grupo exclusivo.   En esta web tendrás toda la información de la actividad del grupo: histórica, presente y futura.   Que Dios te ayude ");
