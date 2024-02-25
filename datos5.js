1c3aff4f758c80cd50cbd787ece9dd26a45f8e15
Gatoromerogolf <ruben.e.garcia@gmail.com>
2024-02-25 20:17:13 -0300

    ordendiario

---
+++ datos5.js
@@ -0,0 +1,7 @@
+const fechas = [
+    {fec:1, dia:'Sábado', diaFecha:3, mesFecha:2, textoFecha:"Sábado 3 de Febrero"},
+    {fec:2, dia:'Sábado', diaFecha:3, mesFecha:2, textoFecha:"Sábado 10 de Febrero"},
+    {fec:3, dia:'Martes', diaFecha:3, mesFecha:2, textoFecha:"Martes 13 de Febrero"},
+    {fec:4, dia:'Sábado', diaFecha:3, mesFecha:2, textoFecha:"Sábado 17 de Febrero"},
+    {fec:5, dia:'Sábado', diaFecha:3, mesFecha:2, textoFecha:"Sábado 24 de Febrero"}
+]
