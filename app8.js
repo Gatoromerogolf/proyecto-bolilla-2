752011528d9eedbb75de2d438657f320dd0e4524
Gatoromerogolf <ruben.e.garcia@gmail.com>
2024-02-26 15:46:54 -0300

    caso8

---
+++ app8.js
@@ -0,0 +1,21 @@
+let matrizSeis = [];
+matrizSeis [0][0] = "Nadie";
+// for jugador of players2{
+
+let filaSeis = 
+
+for (i=0; i< 12; i++){}
+    if (matrizSeis[i][0] == jugador.play{
+        filaSeis[i].insertCell(-1)
+        let filaSeis[i].fecha = jugador.fec;
+        let filaSeis[i].neto = jugador.neto
+    }
+    else{
+        matrizSeis.insertRow(-1);
+        filaSeis[i][0] = jugador.play;
+        filaSeis[i].insertCell(-1)
+        let filaSeis[i].fecha = jugador.fec;
+        let filaSeis[i].neto = jugador.neto
+
+    }
+}
