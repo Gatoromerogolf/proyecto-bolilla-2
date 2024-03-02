a7062766d271f5f1a9e7834aa2174220305f8112
Gatoromerogolf <ruben.e.garcia@gmail.com>
2024-03-02 17:12:18 -0300

    actualizo

--- datos5.js
+++ datos5.js
@@ -3,5 +3,6 @@ const fechas = [
     {fec:2, dia:'Sábado', diaFecha:3, mesFecha:2, textoFecha:"Sábado 10 de Febrero"},
     {fec:3, dia:'Martes', diaFecha:3, mesFecha:2, textoFecha:"Martes 13 de Febrero"},
     {fec:4, dia:'Sábado', diaFecha:3, mesFecha:2, textoFecha:"Sábado 17 de Febrero"},
-    {fec:5, dia:'Sábado', diaFecha:3, mesFecha:2, textoFecha:"Sábado 24 de Febrero"}
+    {fec:5, dia:'Sábado', diaFecha:3, mesFecha:2, textoFecha:"Sábado 24 de Febrero"},
+    {fec:6, dia:'Sábado', diaFecha:2, mesFecha:3, textoFecha:"Sábado 2 de Marzo"}
 ]
